3d66c74ea20058addd61965794d6e28c685fb51a
la-strole <sovaleto48@gmail.com>
2020-09-16 12:59:36 +0200

    16-09-2020

--- Valid_Parentheses/valid_parentheses.py
+++ Valid_Parentheses/valid_parentheses.py
@@ -1,4 +1,4 @@
 def valid_parentheses(string):
-    return string.index('(') < string.index(')') and string.count('(') == string.count(')')
+    return string.find('(') < string.find(')') and string.count('(') == string.count(')')
 
 print(valid_parentheses(")("))
