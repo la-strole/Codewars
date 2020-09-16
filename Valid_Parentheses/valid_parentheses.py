e75f862da8087420802118470abc6aac32802197
la-strole <sovaleto48@gmail.com>
2020-09-16 13:07:56 +0200

    16-09-2020

--- Valid_Parentheses/valid_parentheses.py
+++ Valid_Parentheses/valid_parentheses.py
@@ -1,4 +1,12 @@
 def valid_parentheses(string):
-    return string.find('(') < string.find(')') and string.count('(') == string.count(')')
+    result = 0
+    for i in string:
+        if i == '(':
+            result += 1
+        elif i == ')':
+            result -= 1
+        if result < 0:
+            return False
+    return not result
 
-print(valid_parentheses(")("))
+print(valid_parentheses("(())"))
