cd6cf2338cc9770ae15266230e6639e76c8ff702
la-strole <la-strole.sovaleto48@gmail.com>
2021-10-25 11:14:28 +0300

    task: letter box 7 ku

---
+++ letter_box/letter_box.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+
+def letter_box(start: int, end: int):
+    """
+    return list as frequency of digits from 0 to 9 in sequence [start-end]
+    :param start: begin of sequence: int
+    :param end: end of sequence: int
+    :return: result: list
+    """
+    assert isinstance(start, int)
+    assert isinstance(end, int)
+    assert 0 < start < end
+
+    digit_dict = OrderedDict.fromkeys(map(str, range(10)), 0)
+    for box_number in range(start, end + 1):
+        for digit in str(box_number):
+            digit_dict[digit] += 1
+
+    return list(digit_dict.values())
+
+
+def codewars_solution(start, end):
+    result = [0 for _ in range(10)]
+    for box_number in range(start, end + 1):
+        for digit in str(box_number):
+            result[int(digit)] += 1
+    return result
