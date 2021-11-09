87e0799e0bebd46c882b2894b80cf3826c3eddc4
Eugene P <la-strole@users.noreply.github.com>
2021-11-09 16:42:09 +0300

    Update sum_of_pairs.py

--- Sum of pairs/sum_of_pairs.py
+++ Sum of pairs/sum_of_pairs.py
@@ -6,5 +6,7 @@ def sum_pairs(ints, s):
         else:
             answer.add(i)
 
-
-print(sum_pairs([1, 4, 8, 7, 3, 15], 11))
+if __name__ == "__main__":
+    ints = [1, 4, 8, 7, 3, 15]
+    s = 11
+    print(sum_pairs(ints, s))
